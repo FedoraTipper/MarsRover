335aee40ae967fae9250fd6faffee63f445bf17e
FedoraTipper <FedoraTipper@users.noreply.github.com>
2019-01-23 21:10:46 +0200

    Update on code

--- src/main.py
+++ src/main.py
@@ -1,39 +1,106 @@
 import sys
 import re
 
-valid_chars = "0123456789MRLNWSE"
 #List of movements
 movementList = list()
 #Where we can find the rovers
 roverList = list()
 finRoverList = list()
 
-platue = None
 
-def moveRover(line):
+def Move(x, y, line):
+	direction = plateau[x][y]
+	for character in line:
+		xMove = 0
+		yMove = 0
+		if character.lower() == "r": direction = (direction + 1) % 4
+		elif character.lower() == "l": direction = (direction - 1) % 4
+		else: 
+			if direction % 2 == 0:
+				yMove = 1 if direction != 2 else -1
+			else:
+				xMove = 1 if direction != 3 else -1
+			xTemp = x + xMove
+			yTemp = y + yMove
+			if (xTemp >= 0 and xTemp <= length and yTemp >= 0 and yTemp <= height) and (plateau[xTemp][yTemp] == -1):
+					plateau[x][y] = -1
+					x = xTemp
+					y = yTemp
+		plateau[x][y] = direction
+	finRoverList.append([x,y])
 
-#Function used to initalise the size of the platue
-def initPlatue(line):
-	match = re.search(r'\d\s\d', line)
+def Eliminate_Moves():
+	for coords in roverList:
+		if len(movementList) != 0: 
+			move_line = movementList.pop(0)
+			Move(coords[0], coords[1], move_line)
+
+
+#Function to push all move input commands to a list
+def push_to_moveList(line):
+	#Format the line with regex
+	sub_line = re.sub(r'[^MRL]+', '', line)
+	movementList.append(sub_line)
+
+#Create a rover on the map and push it to the rover list to track
+def createRover(x, y, direction, num_direction = -1):
+	if direction.lower() == "n":
+		num_direction = 0
+	elif direction.lower() == "e":
+		num_direction = 1
+	elif direction.lower() == "s":
+		num_direction = 2
+	else:
+		num_direction = 3
+
+	plateau[x][y] = num_direction
+	roverList.append([x,y])
+
+def InputCommands(line):
+	match = re.match(r'\d\s\d\s[NnEeSsWw]', line)
+	if bool(match) and len(line) == 6: 
+		split = match.group(0).split(" ")
+		createRover(int(split[0]),int(split[1]),split[2])
+	else:
+		push_to_moveList(line)
+
+#Function used to initalise the size of the plateau
+def initPlateau(line):
+	match = re.match(r'\d\s\d', line)
 	if bool(match) == False or len(line) != 4: print_command_help() 
-	split = match.group(0).split(" ") 
-	return [[-1 for x in range(int(split[0]))] for y in range(int(split[1]))]
-
-
-def createRover(x, y, dir):
-
-
-def stackCommands(line):
-
+	split = match.group(0).split(" ")
+	global length, height
+	length = int(split[0]) + 1
+	height = int(split[1]) + 1
+	return [[-1 for x in range(length)] for y in range(height)]
 
 def main():
 	if len(sys.argv) != 2:
 		print_runtime_help()
 	with open(sys.argv[1], 'r') as file:
 		lines = file.readlines()
-		platue = initPlatue(lines[0])
+		global plateau
+		plateau = initPlateau(lines[0])
 		for line in lines[1:]:
-			stackCommands(line)
+			InputCommands(line)
+		Eliminate_Moves()
+		print_results()
+
+
+def print_results():
+	for roverCoords in finRoverList:
+		x = roverCoords[0]
+		y = roverCoords[1]
+		direction = ""
+		if plateau[x][y] == 0:
+			direction = " N"
+		elif plateau[x][y] == 1:
+			direction = " E"
+		elif plateau[x][y] == 2:
+			direction = " S"
+		else:
+			direction = " W"
+		print(str(roverCoords[0]) + " " + str(roverCoords[1]) + direction)
 
 def print_runtime_help():
 	print("Run this python file with text file input as the single argument")
